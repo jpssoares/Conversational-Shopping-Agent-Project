8fe0016d919d3d863439e104c3d1250c7d012743
jpssoares <jps.soares@campus.fct.unl.pt>
2023-03-25 14:41:51 +0000

    Cleaned up the code from last commit. Removed repeated code.

--- controller.py
+++ controller.py
@@ -55,6 +55,21 @@ def create_new_recommendation(brand='None', desc='None', id='None', img_path='No
     return recommendation
 
 
+def get_client_search(query_denc):
+    response = client.search(
+        body = query_denc,
+        index = index_name
+    )
+
+    results = [r['_source'] for r in response['hits']['hits']]
+    print('\nSearch results:')
+    results
+    recommendations = get_recommendations(results)
+    
+    responseDict = { "has_response": True, "recommendations":recommendations, "response":"Here are some items I found...", "system_action":""}
+    return responseDict
+
+
 def search_products_with_text_and_attributes(qtxt, search_field='product_main_colour', size_of_query=3):
     query_denc = {
     'size': size_of_query,
@@ -67,41 +82,19 @@ def search_products_with_text_and_attributes(qtxt, search_field='product_main_co
     }
     }
 
-    response = client.search(
-        body = query_denc,
-        index = index_name
-    )
-
-    results = [r['_source'] for r in response['hits']['hits']]
-    print('\nSearch results:')
-    results
-
-    #pp.pprint(results)
-
-    recommendations = get_recommendations(results)
-    
-    responseDict = { "has_response": True, "recommendations":recommendations, "response":"Here are some items I found...", "system_action":""}
-    return responseDict
+    return get_client_search(query_denc)
 
 
 def searching_for_products_with_cross_modal_spaces(search_query, size_of_query=3):
     model = tt.CLIPModel.from_pretrained("openai/clip-vit-base-patch32")
-    #processor = tt.CLIPProcessor.from_pretrained("openai/clip-vit-base-patch32")
     tokenizer = tt.CLIPTokenizer.from_pretrained("openai/clip-vit-base-patch32")
 
     inputs = tokenizer([search_query], padding=True, return_tensors="pt")
     text_features = F.normalize(model.get_text_features(**inputs))
 
-    #inputs = processor(text=[search_query], images=[], return_tensors="pt", padding=True)
-
-    #text_embeds = F.normalize(outputs["text_embeds"])
-
     query_denc = {
     'size': size_of_query,
-    '_source': ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender', 
-                'product_main_colour', 'product_second_color', 'product_brand', 'product_materials', 
-                'product_short_description', 'product_attributes', 'product_image_path', 
-                'product_highlights', 'outfits_ids', 'outfits_products', 'image_embedding'],
+    '_source': product_fields,
     "query": {
             "knn": {
             "combined_embedding": {
@@ -112,18 +105,7 @@ def searching_for_products_with_cross_modal_spaces(search_query, size_of_query=3
         }
     }
 
-    response = client.search(
-        body = query_denc,
-        index = index_name
-    )
-
-    results = [r['_source'] for r in response['hits']['hits']]
-    print('\nSearch results:')
-    results
-    recommendations = get_recommendations(results)
-    
-    responseDict = { "has_response": True, "recommendations":recommendations, "response":"Here are some items I found...", "system_action":""}
-    return responseDict
+    return get_client_search(query_denc)
 
 
 def create_response_for_query(input_query):
