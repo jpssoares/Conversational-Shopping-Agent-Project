4cfd01ba501aa18430790e4c3e852759574d2579
Ricardo <rjo.pereira@campus.fct.unl.pt>
2023-03-27 18:59:00 +0100

    Full text search implemented
    Now it's possible to search for a specific item with a simple text input, for example:
    "red and white shirt" returns some red and white shirts

--- controller.py
+++ controller.py
@@ -2,10 +2,12 @@ from opensearchpy import OpenSearch
 from opensearchpy import helpers
 import os
 import torch.nn.functional as F
-#from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
+# from transformers import CLIPProcessor, CLIPModel, CLIPTokenizer
 import transformers as tt
+import re
 
 from dotenv import load_dotenv
+
 load_dotenv()
 
 # Program variables
@@ -16,25 +18,26 @@ index_name = "farfetch_images"
 user = os.getenv('API_USER')
 password = os.getenv('API_PASSWORD')
 
-product_fields = ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender', 
-                'product_main_colour', 'product_second_color', 'product_brand', 'product_materials', 
-                'product_short_description', 'product_attributes', 'product_image_path', 
-                'product_highlights', 'outfits_ids', 'outfits_products']
+product_fields = ['product_id', 'product_family', 'product_category', 'product_sub_category', 'product_gender',
+                  'product_main_colour', 'product_second_color', 'product_brand', 'product_materials',
+                  'product_short_description', 'product_attributes', 'product_image_path',
+                  'product_highlights', 'outfits_ids', 'outfits_products']
 
 client = OpenSearch(
-    hosts = [{'host': host, 'port': port}],
-    http_compress = True,
-    http_auth = (user, password),
-    url_prefix = 'opensearch',
-    use_ssl = True,
-    verify_certs = False,
-    ssl_assert_hostname = False,
-    ssl_show_warn = False
+    hosts=[{'host': host, 'port': port}],
+    http_compress=True,
+    http_auth=(user, password),
+    url_prefix='opensearch',
+    use_ssl=True,
+    verify_certs=False,
+    ssl_assert_hostname=False,
+    ssl_show_warn=False
 )
+
 
 def get_recommendations(results):
     recommendations = []
-    
+
     for r in results:
         recommendations.append(create_new_recommendation(
             r.get('product_brand'),
@@ -45,41 +48,69 @@ def get_recommendations(results):
 
     return recommendations
 
+
 def create_new_recommendation(brand='None', desc='None', id='None', img_path='None'):
     recommendation = {
-        'brand' : brand,
-        'description' : desc,
-        'id' : id,
-        'image_path' : img_path
+        'brand': brand,
+        'description': desc,
+        'id': id,
+        'image_path': img_path
     }
     return recommendation
 
 
 def get_client_search(query_denc):
     response = client.search(
-        body = query_denc,
-        index = index_name
+        body=query_denc,
+        index=index_name
     )
 
     results = [r['_source'] for r in response['hits']['hits']]
     print('\nSearch results:')
     results
     recommendations = get_recommendations(results)
-    
-    responseDict = { "has_response": True, "recommendations":recommendations, "response":"Here are some items I found...", "system_action":""}
+
+    if len(recommendations) == 0:
+        responseDict = {"has_response": True, "recommendations": recommendations,
+                        "response": "Sorry, I couldn't find any products that meet your query...", "system_action": ""}
+    else:
+        responseDict = {"has_response": True, "recommendations": recommendations,
+                        "response": "Here are some items I found...", "system_action": ""}
+
     return responseDict
 
 
 def search_products_with_text_and_attributes(qtxt, search_field='product_main_colour', size_of_query=3):
     query_denc = {
-    'size': size_of_query,
-    '_source': product_fields,
-    'query': {
-        'multi_match': {
-        'query': qtxt,
-        'fields': [search_field]
+        'size': size_of_query,
+        '_source': product_fields,
+        'query': {
+            'multi_match': {
+                'query': qtxt,
+                'fields': [search_field]
+            }
         }
     }
+
+    return get_client_search(query_denc)
+
+
+def search_products_full_text(qtxt: str):
+    qtxt = re.sub("\s*(not|no|without)\s*", "-", qtxt)
+    qtxt = re.sub("\s*or\s*", "|", qtxt)
+    qtxt = re.sub("\s*and\s*|\s+", "+", qtxt)
+    print(qtxt)
+    query_denc = {
+        'size': 5,
+        '_source': product_fields,
+        'query': {
+            'simple_query_string': {
+                'query': qtxt,
+                'fields': ['product_family', 'product_category', 'product_sub_category', 'product_gender',
+                           'product_main_colour', 'product_second_color', 'product_brand', 'product_materials',
+                           'product_short_description', 'product_attributes', 'product_image_path']
+            }
+        }
     }
 
     return get_client_search(query_denc)
@@ -93,14 +124,14 @@ def searching_for_products_with_cross_modal_spaces(search_query, size_of_query=3
     text_features = F.normalize(model.get_text_features(**inputs))
 
     query_denc = {
-    'size': size_of_query,
-    '_source': product_fields,
-    "query": {
+        'size': size_of_query,
+        '_source': product_fields,
+        "query": {
             "knn": {
-            "combined_embedding": {
-                "vector": text_features[0].detach().numpy(),
-                "k": 2
-            }
+                "combined_embedding": {
+                    "vector": text_features[0].detach().numpy(),
+                    "k": 2
+                }
             }
         }
     }
@@ -110,8 +141,8 @@ def searching_for_products_with_cross_modal_spaces(search_query, size_of_query=3
 
 def create_response_for_query(input_query):
     input_query_parts = input_query.split(' ')
-    
-    if len(input_query_parts)==2:
+
+    if len(input_query_parts) == 2:
         if input_query_parts[0] in product_fields:
             """
             Search for Products with Text and Attributes 
@@ -126,5 +157,6 @@ def create_response_for_query(input_query):
             Example: black boots
             """
             return searching_for_products_with_cross_modal_spaces(input_query)
+    else:
+        return search_products_full_text(input_query)
     return None
-    
